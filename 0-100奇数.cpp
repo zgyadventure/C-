dd0af8a9bbf21b6fade619195dcfa44f6a16fc85
zgyadventure <108271028+zgyadventure@users.noreply.github.com>
2022-06-27 21:42:40 +0800

    Add files via upload

---
+++ 0-100奇数.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	while (i<=100)
+	 {
+	   if(i%2==1)
+	    {
+		printf("%d\n",i);                    //´òÓ¡0-100µÄÆæÊý 
+	    }
+	    i++;
+	}
+	return 0;
+	  
+	    
+	  
+}
