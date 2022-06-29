7d0867d545101548d1502724235fd7ca573e0903
zgyadventure <108271028+zgyadventure@users.noreply.github.com>
2022-06-29 18:09:34 +0800

    Add files via upload

---
+++ getchar putchar.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int ch;
+	while ((ch=getchar())!=EOF)                 //getchar放在循环while里和放在外面是不同结果，在外面将变成无限循环. 
+	{											//这里需要输入ctrl+z来结束程序,即ctrl+z=EOF 
+		putchar(ch);                                  
+	}
+	
+	 return 0;
+}
