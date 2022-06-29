7020b2313cd0beae6d63e99edd58436b02316638
zgyadventure <108271028+zgyadventure@users.noreply.github.com>
2022-06-29 23:04:21 +0800

    Add files via upload

---
+++ 输入密码确认密码.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	char password[10]={0};
+	printf("输入密码:");
+	scanf("%s",password);
+	printf("密码二次确认\n");
+	while (int ch=getchar()!='\n');
+	{                                   			//这里用循环不断读取密码以外的部分直到'\n'，防止第二次确认密码直接读取回车 导致任务失败. 
+		;
+	}
+	int password_2=getchar();
+	if(password_2=='y')
+	  printf("二次确认正确");
+	else
+	  printf("二次确认错误");
+	  return 0;
+	 
+	
+	
+} 
