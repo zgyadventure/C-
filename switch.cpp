b233bf52801b54729562278664075f28eb78e7e7
zgyadventure <108271028+zgyadventure@users.noreply.github.com>
2022-07-03 19:28:07 +0800

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -6,7 +6,7 @@ int main()
 	switch(day)
 	{
 	case 1:                                               //这里将1-5，6-7放在一起，不用每一步都用break跳出循环 
-	case 2:							//注意在case后面无break情况下，无论值对不对应都执行					
+	case 2:											
 	case 3:
 	case 4:
 	case 5:
