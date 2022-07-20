64601d5f4d9a71f8bb36f464b1e2b34e214e6905
zgyadventure <108271028+zgyadventure@users.noreply.github.com>
2022-07-20 14:45:48 +0800

    Add files via upload

---
+++ 输入一个数打印乘法表.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	int i;
+	int j;
+	for(i=1;i<=a;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d*%d=%d\t",i,j,i*j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
